7dd1abf1cd5a335849ba538b21a6409cf20aa3a3
kumar6646 <sai.penusoft@gmail.com>
2025-07-05 10:40:56 +0530

    Add project files.

---
+++ FirstConsoleAppsn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleAppsn
+{
+    /*
+     * Varibale is the name of memory location.
+     * Variable is used to store data.
+     * Identifier is the name of variable.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age=100;
+            Console.WriteLine("value of a is {0}", age);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
